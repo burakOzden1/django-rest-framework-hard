a1a8e9f4d321421ee0d28408cc1f81aab97f6235
Zeki Burak Özden <zburakozden@gmail.com>
2024-04-29 22:07:26 +0300

    ViewSets ve Routers mantigini anlama

--- profiller/api/urls.py
+++ profiller/api/urls.py
@@ -1,6 +1,21 @@
-from django.urls import path
-from profiller.api import views
+from django.urls import path, include
+from profiller.api.views import ProfilViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'kullanici-profilleri', ProfilViewSet)
 
 urlpatterns = [
-    path('kullanici-profilleri/', views.ProfilList.as_view(), name='profiller')
+    path('', include(router.urls))
 ]
+
+## HER DEFASINDA BUNLARI YAZMAK ZORUNDA DEGILIZ!!!!!!!
+# from profiller.api.views import ProfilViewSet
+
+# profil_list = ProfilViewSet.as_view({'get': 'list'})
+# profil_detay = ProfilViewSet.as_view({'get': 'retrieve'})
+
+# urlpatterns = [
+#     path('kullanici-profilleri/', profil_list, name='profiller'),
+#     path('kullanici-profilleri/<int:pk>', profil_detay, name='profil-detay'),
+# ]
