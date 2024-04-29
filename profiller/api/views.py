f40fb28843b7e7d3a57a32b7e3155b9b7d2af1ee
Zeki Burak Özden <zburakozden@gmail.com>
2024-04-29 23:35:16 +0300

    profil durumu icin views lar yazdik.

--- profiller/api/views.py
+++ profiller/api/views.py
@@ -1,11 +1,11 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from profiller.models import Profil
-from profiller.api.serializers import ProfilSerializer
-# from rest_framework.viewsets import ReadOnlyModelViewSet
+from profiller.models import Profil, ProfilDurum
+from profiller.api.serializers import ProfilSerializer, ProfilDurumSerializer
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.viewsets import GenericViewSet
 from rest_framework import mixins
-from profiller.api.permissions import KendiProfiliYaDaReadOnly
+from profiller.api.permissions import KendiProfiliYaDaReadOnly, DurumSahibiYaDaReadOnly
 
 
 class ProfilViewSet(
@@ -24,3 +24,14 @@ class ProfilViewSet(
 #     queryset = Profil.objects.all()
 #     serializer_class = ProfilSerializer
 #     permission_classes = [IsAuthenticated]
+
+
+
+class ProfilDurumViewSet(ModelViewSet):
+    queryset = ProfilDurum.objects.all()
+    serializer_class = ProfilDurumSerializer
+    permission_classes = [IsAuthenticated, DurumSahibiYaDaReadOnly]
+
+    def perform_create(self, serializer):
+        user_profil = self.request.user.profil
+        serializer.save(user_profil=user_profil)
